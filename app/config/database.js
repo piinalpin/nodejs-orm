ac0c47554e2c37d02927a30a11dd150ba83e7e2a
Alvinditya Saputra <alvindityas@gmail.com>
2021-12-07 21:18:55 +0700

    add database configuration

---
+++ app/config/database.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
+dotenv.config();
+
+const db = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
+)
+
+export default db;
